5c53b446921a8081027f9e8a13841e0d30babe82
Rickson <ricksonrocha0@gmail.com>
2022-12-23 13:40:39 -0300

    :sparkles: Add a describve

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -2,21 +2,24 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Formulario from './index'
 
-test('Render a input', () => {
-    render(<Formulario/>)
-    const input = screen.getByPlaceholderText('Digite um valor')
-    expect(input).toBeInTheDocument()
-})
+describe('Render a input', () => {
 
-test('Render a input type number', () => {
-    render(<Formulario/>)
-    const inputNumber = screen.getByPlaceholderText('Digite um valor')
-    expect(inputNumber).toHaveAttribute('type', 'number')
-})
-
-test('Render a input type text', () => {
-    render(<Formulario/>)
-    const inputNumber = screen.getByPlaceholderText('Digite um valor')
-    userEvent.type(inputNumber, '50')
-    expect(inputNumber).toHaveValue(50)
+    test('in document', () => {
+        render(<Formulario/>)
+        const input = screen.getByPlaceholderText('Digite um valor')
+        expect(input).toBeInTheDocument()
+    })
+    
+    test('type number', () => {
+        render(<Formulario/>)
+        const inputNumber = screen.getByPlaceholderText('Digite um valor')
+        expect(inputNumber).toHaveAttribute('type', 'number')
+    })
+    
+    test('type text', () => {
+        render(<Formulario/>)
+        const inputNumber = screen.getByPlaceholderText('Digite um valor')
+        userEvent.type(inputNumber, '50')
+        expect(inputNumber).toHaveValue(50)
+    })
 })
